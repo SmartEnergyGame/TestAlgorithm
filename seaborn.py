f21e308f0202ac51908bad6a22ae682dadced65a
Daniel Santos <dfsantosbu@unal.edu.co>
2018-02-27 13:46:40 -0500

    Add first snipet to test seaborn algorithm.

---
+++ seaborn.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import seaborn as sns
+
+sns.set()
+
+# Load the example iris dataset
+iris = sns.load_dataset("iris")
+
+# "Melt" the dataset to "long-form" or "tidy" representation
+iris = pd.melt(iris, "species", var_name="measurement")
+
+# Draw a categorical scatterplot to show each observation
+sns.swarmplot(x="measurement", y="value", hue="species", data=iris)
